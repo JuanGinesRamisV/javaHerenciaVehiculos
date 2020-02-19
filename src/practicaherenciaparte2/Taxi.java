786ab6cbc6c9393670152721f0ca59eb091f9a94
juan gines ramis vivancos <jramisv@cifpfbmoll.eu>
2020-02-19 16:10:57 +0100

    initial commit vehiculos

---
+++ src/practicaherenciaparte2/Taxi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaherenciaparte2;
+
+/**
+ *
+ * @author juang
+ */
+public class Taxi extends Vehiculo{
+    private String numeroLicencia;
+    private boolean estado;
+    private int numeroTaxistas;
+    
+    
+    
+}
+
+
---
+++ src/practicaherenciaparte2/Vehiculo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaherenciaparte2;
+
+/**
+ *
+ * @author juang
+ */
+public class Vehiculo {
+    private static int contadorId;
+    private int id;
+    private String matriula;
+    private String vehiculo;
+    private int potencia;
+    
+    
+}
